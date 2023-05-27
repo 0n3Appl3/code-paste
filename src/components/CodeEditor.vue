477d3fcc5bd5c2bee00e4ebbe871361013d19403
0n3Appl3 <jedd.lupoy@gmail.com>
2023-05-27 15:01:46 +1200

    Resolved issues related to caret position

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-
-const hello = ref('Hello world!')
+import hljs from 'highlight.js'
 
 // Editor Container Styling
 const container = ref('editor__container')
@@ -19,10 +18,73 @@ const textInput = ref(null)
 
 const resize = () => {
     const element = textInput.value
+    const caretPosition = getCaretCharacterOffsetWithin()
+    const div = document.createElement('div')
+    div.innerHTML = hljs.highlightAuto(element.innerText).value
+    element.textContent = null
+    element.appendChild(div)
+    setCaretPosition(element, caretPosition)
     element.style.height = 'auto'
     element.style.height = element.scrollHeight + 'px'
     element.style.width = 'auto'
     element.style.width = (element.scrollWidth + 15) + 'px'
+}
+
+const enter = () => {
+    setTimeout(function() {
+        setCaretPosition(textInput.value, getCaretCharacterOffsetWithin() + 1)
+    }, 1)
+}
+
+// Solution credit: https://stackoverflow.com/questions/36869503/set-caret-position-in-contenteditable-div-that-has-children
+const setCaretPosition = (element, position) => {
+    for (let node of element.childNodes) {
+        if (node.nodeType == Node.TEXT_NODE) {
+            if (node.length >= position) {
+                const range = document.createRange()
+                const selection = window.getSelection()
+                range.setStart(node, position)
+                range.collapse(true)
+                selection.removeAllRanges()
+                selection.addRange(range)
+                return -1
+            } else {
+                position -= node.length
+            }
+        } else {
+            position = setCaretPosition(node, position)
+            if (position == -1) {
+                return -1
+            }
+        }
+    }
+    return position;
+}
+
+// Solution credit: http://jsfiddle.net/TjXEG/900/
+const getCaretCharacterOffsetWithin = () => {
+    const element = textInput.value
+    let caretOffset = 0
+    const doc = element.ownerDocument || element.document
+    const win = doc.defaultView || doc.parentWindow
+    let sel = ''
+    if (typeof win.getSelection != "undefined") {
+        sel = win.getSelection()
+        if (sel.rangeCount > 0) {
+            const range = win.getSelection().getRangeAt(0)
+            const preCaretRange = range.cloneRange()
+            preCaretRange.selectNodeContents(element)
+            preCaretRange.setEnd(range.endContainer, range.endOffset)
+            caretOffset = preCaretRange.toString().length
+        }
+    } else if ( (sel = doc.selection) && sel.type != "Control") {
+        const textRange = sel.createRange()
+        const preCaretTextRange = doc.body.createTextRange()
+        preCaretTextRange.moveToElementText(element)
+        preCaretTextRange.setEndPoint("EndToEnd", textRange)
+        caretOffset = preCaretTextRange.text.length
+    }
+    return caretOffset
 }
 </script>
 
@@ -33,7 +95,11 @@ const resize = () => {
             <div :class="toolbarMinimise"></div>
             <div :class="toolbarMaximise"></div>
         </div>
-        <textarea @input="resize" ref="textInput" name="code" :class="textField" placeholder="Enter your code"></textarea>
+        <!-- <textarea @input="resize" ref="textInput" name="code" :class="textField" placeholder="Enter your code"></textarea> -->
+        <div v-on:keydown.enter="enter"
+             @input="resize" ref="textInput" 
+             contenteditable="true" 
+             :class="textField"></div>
     </div>
 </template>
 
@@ -70,12 +136,13 @@ const resize = () => {
 .editor__text-field {
     display: block;
     background-color: #1b3038;
+    font-family: 'Courier New', Courier, monospace;
     outline: none;
     resize: none;
     border: none;
     padding: 0.8rem;
     color: #eee;
-    white-space: nowrap;
+    white-space: pre;
     overflow: hidden;
 }
 .editor__text-field::placeholder {
